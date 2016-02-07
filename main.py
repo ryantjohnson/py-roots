dc3d2a7aaab871a0b072728c8d35c094f0b3f87d
rjsaxman@gmail.com <rjsaxman@gmail.com>
2016-02-06 18:21:50 -0800

    got all links working

--- main.py
+++ main.py
@@ -4,8 +4,32 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def main():
+def index():
 	return render_template('index.html')
+
+@app.route("/about")
+def about():
+	return render_template('about.html')
+
+@app.route("/contact")
+def contact():
+	return render_template('contact.html')
+
+@app.route("/events")
+def events():
+	return render_template('events.html')
+
+@app.route("/staff")
+def staff():
+	return render_template('staff.html')
+
+@app.route("/venue")
+def venue():
+	return render_template('venue.html')
+
+@app.route("/affiliates")
+def affiliates():
+	return render_template('affiliates.html')
 
 if __name__ == "__main__":
 	app.run(debug=True, port=5000)
